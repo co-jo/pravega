7240961398632ae7cf6b53b88ccb2b8185d00086
co-jo <colin.hryniowski@dell.com>
2020-10-21 15:42:34 -0700

    Test.
    Signed-off-by: co-jo <colin.hryniowski@dell.com>

--- InetTests.java
+++ InetTests.java
@@ -1,9 +1,12 @@
 import java.net.Inet4Address;
 import java.net.UnknownHostException;
+import java.net.InetSocketAddress;
 
 class InetTests {
 
     public static void main(String args[]) throws UnknownHostException {
         System.out.println(Inet4Address.getLocalHost().getHostAddress());
+        InetSocketAddress inetAddr = new InetSocketAddress("localhost", 3181);
+        System.out.println(InetAddress.getByName("2c1b9d5bfb2c").getHostName());
     }
 }
