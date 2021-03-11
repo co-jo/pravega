5b007fc0b2c33e075e4aa5eecdb4310fdcbf92b4
co-jo <colin.hryniowski@dell.com>
2021-03-11 14:26:15 -0800

    avoid conflict.

--- test/integration/src/test/java/io/pravega/test/integration/StreamMetricsTest.java
+++ test/integration/src/test/java/io/pravega/test/integration/StreamMetricsTest.java
@@ -160,9 +160,9 @@ public class StreamMetricsTest {
 
     @Test(timeout = 30000)
     public void testStreamsAndScopesBasicMetricsTests() throws Exception {
-        Collection<Counter> counters = MetricRegistryUtils.getCounters(MetricsNames.CREATE_SCOPE_FAILED);
-        log.info("Counters Before@@@: {}", counters.size());
-        for (val counter : counters) {
+        Collection<Counter> countersZero = MetricRegistryUtils.getCounters(MetricsNames.CREATE_SCOPE_FAILED);
+        log.info("Counters Before@@@: {}", countersZero.size());
+        for (val counter : countersZero) {
             log.info("counter Before: {} {}", counter.getId(), counter.count());
         }
         String scopeName = "scopeBasic";
